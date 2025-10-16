42de50ffb65a30d1e0d503ad08fbd4ececded1a8
kma <kma@objctv.ru>
2025-10-16 18:40:04 +0500

    тестовый контроллер для фронта

---
+++ API/Controllers/TesterController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/api/")]
+public class WeatherForecastController : ControllerBase
+{
+    
+
+    [HttpGet("test")]
+    public IActionResult Get()
+    {
+        return Ok("Йоу - это гет");
+    }
+    [HttpPost("test")]
+    public IActionResult Post()
+    {
+        return Ok("йоу - это пост");
+    }
+
+    [HttpPut("test")]
+    public IActionResult Put()
+    {
+        return Ok("йоу - это пут");
+    }
+
+    [HttpDelete("test")]
+    public IActionResult Delete()
+    {
+        return Ok("йоу - это делете");
+    }
+
+}
