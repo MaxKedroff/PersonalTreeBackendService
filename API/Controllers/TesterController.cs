124f17a09f84f6f79a9bea3d06ea60b0a3c59197
kma <kma@objctv.ru>
2025-10-16 21:19:47 +0500

    тест контроллера для видео

--- API/Controllers/TesterController.cs
+++ API/Controllers/TesterController.cs
@@ -13,6 +13,13 @@ public class WeatherForecastController : ControllerBase
     {
         return Ok("йоу - это гет");
     }
+
+    [HttpGet("test2")]
+    public IActionResult Get2()
+    {
+        return Ok("йоу - это второй гет");
+    }
+
     [HttpPost("test")]
     public IActionResult Post()
     {
