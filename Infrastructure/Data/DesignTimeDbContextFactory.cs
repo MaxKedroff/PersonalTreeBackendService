fe3e3df9ca64c9b87eeabcf65a179861cdd183f4
kma <kma@objctv.ru>
2025-10-23 20:59:47 +0500

    добавил миграции к бд и настройку чтобы они появлялись в инфраструктурном слое

---
+++ Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<UserDb>
+    {
+        public UserDb CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserDb>();
+
+            var connectionString = "Host=localhost;Port=5432;Database=persontree;Username=postgres;Password=postgres";
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new UserDb(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20251023155859_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    User_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Manager_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    Is_admin = table.Column<bool>(type: "boolean", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    patronymic = table.Column<string>(type: "text", nullable: false),
+                    birth_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    interests = table.Column<string>(type: "text", nullable: false),
+                    position = table.Column<string>(type: "text", nullable: false),
+                    department = table.Column<string>(type: "text", nullable: false),
+                    work_exp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    avatar = table.Column<string>(type: "text", nullable: false),
+                    new_avatar = table.Column<string>(type: "text", nullable: false),
+                    Contacts = table.Column<string>(type: "jsonb", nullable: false),
+                    SamAccountName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    LastAdSync = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AdGuid = table.Column<string>(type: "text", nullable: false),
+                    Created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ManagerUser_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.User_id);
+                    table.ForeignKey(
+                        name: "FK_users_users_ManagerUser_id",
+                        column: x => x.ManagerUser_id,
+                        principalTable: "users",
+                        principalColumn: "User_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_ManagerUser_id",
+                table: "users",
+                column: "ManagerUser_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
