952509f95993450f84b086b2ac186173800513c8
kma <kma@objctv.ru>
2025-11-08 19:38:35 +0500

    добавил миграции бд

---
+++ Infrastructure/Migrations/20251108143820_ImplRolesToUserDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplRolesToUserDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Is_admin",
+                table: "users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Is_admin",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
