32ad5148ec9173314480213c33624e30e43cde3f
kma <kma@objctv.ru>
2025-12-16 19:37:47 +0500

    чуть чуть подшаманил над хранением массива в бд

---
+++ Infrastructure/Migrations/20251216143718_ChangeSkillsToPostgreSQLArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSkillsToPostgreSQLArray : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string[]>(
+                name: "Skills",
+                table: "users",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "jsonb");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "Skills",
+                table: "users",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(string[]),
+                oldType: "text[]");
+        }
+    }
+}
